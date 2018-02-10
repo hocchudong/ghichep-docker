43a4825c17dc17d433bbbd0bb1035be8e1fbc0ff
CongTO <tcvn1985@gmail.com>
2018-02-10 16:55:46 +0700

    script cai dat docker
    script cai dat docker

---
+++ scripts/install-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Cai dat docker"
+sleep 3
+curl -sSL https://get.docker.com/| sudo sh
+sudo usermod -aG docker `whoami`
+
+systemctl start docker.service
+systemctl enable docker.service
+
+
+echo "Cai dat docker compose"
+sleep 3
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sleep 3
+echo "Kiem tra trang thai docker va docker compose"
+systemctl status | grep docker*
+docker version 
+docker-compose --version
+##############
+echo "IMOK"
