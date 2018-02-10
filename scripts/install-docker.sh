cba9b21374156451d83d080ed6aebb76bb87d4bd
CongTO <tcvn1985@gmail.com>
2018-02-10 16:56:19 +0700

    script cai dat docker

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -4,14 +4,11 @@ echo "Cai dat docker"
 sleep 3
 curl -sSL https://get.docker.com/| sudo sh
 sudo usermod -aG docker `whoami`
-
 systemctl start docker.service
 systemctl enable docker.service
 
-
 echo "Cai dat docker compose"
 sleep 3
-
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
